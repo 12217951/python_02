f2eb67da4ee7b5438806372daea1a2d1bdbf142e
Smita <114800220+12217951@users.noreply.github.com>
2023-01-07 14:26:51 +0530

    Add files via upload

---
+++ functions.py/string_functions.py
@@ -0,0 +1,16 @@
+w="123welcome"
+# lower()
+# upper()
+# title()
+# capitalize()
+print(w.find("e",2))# find()-----> find returns -1 if letter not found and .index() returns error.
+print(w.isalpha())# isalpha()
+print(w.isdigit())# isdigit()
+print(w.isalnum())# isalnum()
+
+
+a=67
+h="A"     
+print(chr(a))
+# chr()
+print(ord(h))# ord()
